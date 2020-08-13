11e0ad5dca005f886441dfb8a90f18e7ac852aeb
anmona412 <anmo.knight412@gmail.com>
2020-08-13 14:58:13 +0900

    ActiveHash設定

--- app/models/age.rb
+++ app/models/age.rb
@@ -1,2 +1,11 @@
-class Age < ApplicationRecord
+class Age < Activehash::Base
+  self.data = [
+    { id: 1, age: '---' },
+    { id: 2, age: '20才以下' },
+    { id: 3, age: '20~30才' },
+    { id: 4, age: '30~40才' },
+    { id: 5, age: '40~50才' },
+    { id: 6, age: '50~60才' },
+    { id: 7, age: '60才以上' }
+  ]
 end
