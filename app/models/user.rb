d222eac76be53290a8bdbbadd10bde0b83e408d6
anmona412 <anmo.knight412@gmail.com>
2020-08-13 15:35:38 +0900

    sns_credentialモデル設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,10 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:facebook, :google_oauth2]
 
   # assosiation
+  has_many :sns_credentials
   belongs_to_active_hash :age
   belongs_to_active_hash :profession
 
