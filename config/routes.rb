dad55da7e9f0c653fa51dc2558f1e383b7425f98
anmona412 <anmo.knight412@gmail.com>
2020-08-13 16:01:46 +0900

    deviseコントローラ再設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    omniauth_callbacks: 'users/omniauth_callbacks',
+    registrations: 'users/registrations'
+  }
+  
   root 'users#index'
   resources :users
-  #[]内最後のカンマは消すこと
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
