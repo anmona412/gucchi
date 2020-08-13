9e8f49315b07f52fe67f7693775751a053e76499
anmona412 <anmo.knight412@gmail.com>
2020-08-13 14:58:27 +0900

    Userモデル設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,16 @@
 class User < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # assosiation
+  belongs_to_active_hash :age
+  belongs_to_active_hash :profession
+
+  # validation
+  with_options numericality: { other_than: 1 } do
+    validates :age_id, :profession_id
+  end
 end
