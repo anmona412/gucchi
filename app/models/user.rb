bc69afc9f88aab15db453823ab507071d1e248e7
anmona412 <anmo.knight412@gmail.com>
2020-08-13 16:59:22 +0900

    sns登録機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,19 @@ class User < ApplicationRecord
   belongs_to_active_hash :age
   belongs_to_active_hash :profession
 
+  def self.from_omniauth(auth)
+    sns = SnsCredential.where(provider: auth.provider, uid: auth.uid).first_or_create
+    user = User.where(email: auth.info.email).first_or_initialize(
+      nickname: auth.info.name,
+        email: auth.info.email
+    )
+    if user.persisted?
+      sns.user = user
+      sns.save
+    end
+    {user: user, sns: sns}
+  end
+
   # validation
   with_options numericality: { other_than: 1 } do
     validates :age_id, :profession_id
