e9c367986a2313fe9424437bb24b634fadf7bced
anmona412 <anmo.knight412@gmail.com>
2020-08-13 13:00:08 +0900

    devise用コントローラ

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root 'users#index'
+  resources :users, only: [:index]
+  #[]内最後のカンマは消すこと
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
