b5e198bbe901aa2610c8d66b1b03a7b0c8aba88a
anmona412 <anmo.knight412@gmail.com>
2020-09-02 12:41:52 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,11 @@
-# config valid for current version and patch releases of Capistrano
-lock "~> 3.14.1"
+# capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
+lock '3.14.1'
 
-set :application, "gucchi"
-set :repo_url, "git@github.com:anmona412/gucchi.git"
+# Capistranoのログの表示に利用する
+set :application, 'gucchi'
+
+# どのリポジトリからアプリをpullするかを指定する
+set :repo_url,  'git@github.com:anmona412/gucchi.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
